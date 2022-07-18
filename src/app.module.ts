6e00569d33470bc22444045b3854e4d56985a820
Gaurav Tayal <gauravtayal2013@gmail.com>
2022-07-18 21:48:21 +0530

    add code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://root:9bdd7j0JlNOmuOW1@uscities.fzouh.mongodb.net/cities',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
