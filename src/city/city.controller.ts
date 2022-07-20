5a42fba8d0955ccbecacdb4071d14c2a4254d684
Gaurav Tayal <gauravtayal2013@gmail.com>
2022-07-20 20:45:53 +0530

    add search condition and other data

--- src/city/city.controller.ts
+++ src/city/city.controller.ts
@@ -1,13 +1,26 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { CityService } from './city.service';
 import { CreateCityDto } from './dto/create-city.dto';
 import { City } from './city.schema';
+import { Request, Response } from 'express';
 
 @Controller('city')
 export class CityController {
   constructor(private readonly cityService: CityService) {}
-  @Get()
-  async findAll(): Promise<City[]> {
-    return this.cityService.findAll();
+  @Get('/suggestions')
+  async findAll(@Req() req: Request, @Res() res: Response) {
+    const queryData = req.query;
+    console.log('queryData', queryData);
+    const resData = await this.cityService.findAll(queryData);
+    res.status(200).send({ suggestions: resData });
   }
 }
--- src/city/city.service.ts
+++ src/city/city.service.ts
@@ -10,7 +10,21 @@ export class CityService {
     @InjectModel(City.name) private readonly cityModel: Model<CityDocument>,
   ) {}
 
-  async findAll(): Promise<City[]> {
-    return this.cityModel.find().exec();
+  async findAll(queryParam): Promise<City[]> {
+    return this.cityModel
+      .find({
+        name: { $regex: queryParam.q, $options: 'i' },
+        distance: {
+          $geoNear: {
+            $maxDistance: 5000,
+            $geometry: {
+              type: 'Point',
+              coordinates: [queryParam.latitude, queryParam.longitude],
+            },
+          },
+        },
+      })
+      .select({ name: 1, long: 1, lat: 1, distance: 1 })
+      .exec();
   }
 }
